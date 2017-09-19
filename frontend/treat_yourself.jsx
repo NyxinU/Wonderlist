fc55aaf6fd132e651c8815fd399fccede9e3affb
Nix <yiunixon@gmail.com>
2017-09-19 15:26:20 -0700

    Did phase 0 setup

--- frontend/treat_yourself.jsx
+++ frontend/treat_yourself.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Remember to treat yourself! You'll thank yourself later</h1>, root);
+  ReactDOM.render( <h1>Don't Forget to Treat Yorself! </h1> , root);
 });
