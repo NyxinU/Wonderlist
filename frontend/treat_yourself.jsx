a21bdf92c86c22f76933f2f77454eb2e03b83925
Nix <yiunixon@gmail.com>
2017-09-19 13:57:47 -0700

    Did initial setup

---
+++ frontend/treat_yourself.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Remember to treat yourself! You'll thank yourself later</h1>, root);
+});
