1a5bd27aa8be7ca7b0f3469305f77e062ed2bdb5
Nix <yiunixon@gmail.com>
2017-09-20 15:27:40 -0700

    Wrtie greeting

---
+++ frontend/components/greeting/greeting.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Greeting = ({ currentUser, logout }) => (
+  currentUser ? welcomeMessage(currentUser, logout) : sessionLinks()
+);
+
+const welcomeMessage = (currentUser, logout) => (
+  <div>
+    <h2>Hi, {currentUser.first_name}!</h2>
+    <button onClick={logout}>Log Out</button>
+  </div>
+);
+
+const sessionLinks = () => (
+  <div>
+    <Link to="/login">Log in</Link>
+    &nbsp; | &nbsp;
+    <Link to="/signup">Sign up</Link>
+  </div>
+);
+
+export default Greeting;
