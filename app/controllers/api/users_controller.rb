51eb3b4880343fabc05fe3e69eb8e8768e8d3fce
Nix <yiunixon@gmail.com>
2017-09-20 10:19:10 -0700

    Write Auth Backend

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,18 +1,21 @@
 class Api::UsersController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
   def create
-    @user = user.new(user_params)
+    @user = User.new(user_params)
 
     if @user.save
       login(@user)
       render "api/users/show"
     else
-      render json: @user.errors.full_mesages, status: 422
+      render json: @user.errors.full_messages, status: 422
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :email, :first_name, :last_name)
   end
 end
