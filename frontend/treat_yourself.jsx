614a5cabf13be0139dc3441658b4db118638005a
Nix <yiunixon@gmail.com>
2017-09-20 14:32:43 -0700

    Write app.jsx and root.jsx

--- frontend/treat_yourself.jsx
+++ frontend/treat_yourself.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as SessionApiUtil from './frontend/actions/session_actions';
+import * as SessionApiUtil from './actions/session_actions';
+import configureStore from './store/store';
+import Root from './components/root';
 
-window.login = SessionApiUtil.login;
-window.logout = SessionApiUtil.logout;
-window.signup = SessionApiUtil.signup;
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+// testing start
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.login = SessionApiUtil.login;
+  window.logout = SessionApiUtil.logout;
+  window.signup = SessionApiUtil.signup;
+// testing end
+
   const root = document.getElementById('root');
-  ReactDOM.render( <h1>Don't Forget to Treat Yourself! </h1> , root);
+  ReactDOM.render( <Root store={ store }/> , root);
 });
