750d3c32b793928cf98a6beb4d3f7e9d5d13f2e5
Nix <yiunixon@gmail.com>
2017-09-20 17:36:33 -0700

    Write part of session forms

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
+import SessionFormContainer from './session_form/session_form_container';
 
 const App = () => (
   <div>
-    <h2> Hey! </h2>
-    <GreetingContainer />
+    <header>
+      <h2> Hey! </h2>
+      <GreetingContainer />
+    </header>
+    
+    <Route path="/login" component={SessionFormContainer} />
   </div>
 );
 
