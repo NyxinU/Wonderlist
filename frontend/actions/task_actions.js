87eba57df3437287a80886a8b1889ac96c9539bb
Nix <yiunixon@gmail.com>
2017-09-22 19:01:11 -0700

    Write task actions

---
+++ frontend/actions/task_actions.js
@@ -0,0 +1,48 @@
+import * as TaskUtil from '../util/task_util';
+
+export const RECEIVE_ALL_TASKS = 'RECEIVE_ALL_TASKS';
+export const RECEIVE_TASK = 'RECEIVE_TASK';
+export const REMOVE_TASK = 'REMOVE_TASK';
+export const RECEIVE_TASK_ERRORS = 'RECEIVE_TASK_ERRORS';
+
+export const requestAllTasks = () => dispatch => (
+  TaskUtil.fetchTasks().then(tasks => dispatch(receiveAllTasks(tasks)))
+);
+
+export const requestTask = () => dispatch => (
+  TaskUtil.fetchTask().then(task => dispatch(receiveTask(task)))
+);
+
+export const createTask = () => dispatch => (
+  TaskUtil.createTask().then(task => dispatch(receiveTask(task)))
+);
+
+export const updateTask = () => dispatch => (
+  TaskUtil.updateTask().then(task => dispatch(receiveTask(task)))
+);
+
+export const deleteTask = (id) => dispatch => (
+  TaskUtil.destroyTask(id).then(task => dispatch(removeTask(task)))
+);
+
+
+
+export const receiveAllTasks = tasks => ({
+  type: RECEIVE_ALL_TASKS,
+  tasks
+});
+
+export const receiveTask = task => ({
+  type: RECEIVE_TASK,
+  task
+});
+
+export const removeTask = task => ({
+  type: REMOVE_TASK,
+  task
+})
+
+export const receiveTaskErrors = errors => ({
+  type: RECEIVE_TASK_ERRORS,
+  errors
+});
